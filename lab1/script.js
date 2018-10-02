d125d1ae8903304670b1efb0ce626d5eda2dfcb7
bishwa123 <bishwakhatri123@gmail.com>
2018-10-02 00:42:22 -0500

    lab01 prob 3

--- lab1/script.js
+++ lab1/script.js
@@ -1,6 +1,8 @@
 
 (function(){
     'use strict'
+
+
     //problem 1 using ES6 feature
     const uniArr = [4,1,5,7,2,3,1,4,6,5,2];
     console.log(`Original Array ${uniArr}`);
@@ -11,6 +13,8 @@
     function removeDups(arr){
         return [...new Set(arr)];
     }
+
+
     // problem 1 using promises
     var removeDupsPromise = function(data){
         return new Promise((resolve, reject) => {
@@ -18,13 +22,13 @@
             reject(new Error("Operation failed"));
         });
     }
-   
     removeDupsPromise([4,1,5,7,2,3,1,4,6,5,2])
     .then(data => {
         console.log("Removing duplicates by promises...");
         console.log(`Unique Array ${data}`)
     })
     .catch((error) => console.log(error));
+
 
     //problem 1 using async/await
     async function removeDupsAsync(data){
@@ -39,6 +43,7 @@
     }
     removeDupsAsync([4,1,5,7,2,3,1,4,6,5,2]);
 
+
     //problem 1 using observable
     const {from} = rxjs;
    
@@ -48,14 +53,36 @@
            console.log(`Unique Array ${data}`);
         }));
     
+
     //problem 2
     function isWeekend(){
-      //  debugger;
         const todayDate = new Date();
         const day = todayDate.getDay();
-        return (day ==0 || day ==6)?"weekend":"weekday";
+        return (day ===0 || day ===6)?"weekend":"weekday";
     }
     console.log(isWeekend());
+
+    
+    //problem 3
+    let applyCupon = function(category){
+        return function(discount){
+            return function(item){
+                if(item.category === category){
+                    let discountedPrice = item.price - (item.price * discount);
+                    item.price = discountedPrice;
+                    return item;
+                }
+                return null;
+            }
+        }
+    }
+    const item = {
+        name:"Biscuits",
+        type: "regular",
+        category: "food",
+        price : 2.0
+    }
+  console.log(applyCupon("food")(0.1)(item).price);  
 })();
 
 
